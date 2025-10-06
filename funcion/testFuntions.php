40815f80d1b7c38d14469a8fcd8d8ba2992398b5
kelvis23 <jesuscuestas116@gmail.com>
2025-10-06 10:44:52 +0200

    1

---
+++ funcion/testFuntions.php
@@ -0,0 +1,19 @@
+<?php
+//include (wquivalente al import de java para añdir otros ficheros)
+include("./funsiones.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<h1>funcion </h1>
+<body>
+    <?php
+    //llamo al a funcion shownaame
+    showName();
+    ?>
+</body>
+</html>
