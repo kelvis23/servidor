d39286426186fa552d0cab91996e3e3f832320e9
kelvis23 <jesuscuestas116@gmail.com>
2025-10-06 12:48:24 +0200

    4

--- funcion/funsiones.php
+++ funcion/funsiones.php
@@ -1,3 +1,50 @@
 <?php
+function showName(){
+    echo"Kelvis";
+    
+}
+echo"<br>";
+$variable = "hola";
 
+//imprime la suma de dos numeros 
+function printAddition($a,$b){
+echo$a+$b;
+}
+/**
+ * realisa la suma de los numeros enteros o double
+ * @param int|float $a primer operador
+ * @param int|float $b Segundo operador
+ * @return int|float  El resultado 
+ */
+function addition($a,$b):int|float{
+return $a+$b;
+}
+
+//funsion para saludar que puede recivir solamente el nombre (muestra "hola $nombre")
+// o el mombre y el tipo de saludar (muestra $saludo $nombre)
+//pulic static String saludar (String nombre){return "hola"+nombre}
+//pulic static String saludar (String nombre, String saludo){return saludo+ ""+nombre}
+//Esto s java se conoce como sobrescritura de funciones y en php no existe
+
+//oho oarametro con valor por defecto
+function salute($name,$salute = " hola"):string{
+    return$salute ." ". $name;
+}
+
+
+
+/** 
+ * Devuelve como string lo que le digamos como parametro  dentro de las etiquetas que indiquemos (p si no indicamos nada)
+ * @param string $tag etiquetas en las envolver el elmento si no se indica ninguna  que sea <p>por defecto</p>
+ * @param string $element cadena de texto a envolver entre las tags html
+ * @return string con el elemento rodeado de los tags indicados 
+*/
+function intoHem( $element , $tag = "p"):string{
+
+return "<$tag> $element</$tag>";
+}
+
+function matricula($stude,$school,$course= "DaW" ,$year="2025"){
+    return"<p>$stude matriculado en $course, en $year, en el ies $school</p>";
+}
 ?>
--- funcion/testFuntions.php
+++ funcion/testFuntions.php
@@ -4,16 +4,77 @@ include("./funsiones.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <h1>funcion </h1>
+
 <body>
     <?php
     //llamo al a funcion shownaame
     showName();
+    echo "<br>";
+    // se pueden usar las variables declaradas en el fichero 
+    var_dump($variable);
+    echo "<br>";
+    printAddition(7, 9);
+    $number = 1;
+    $number1 = 2;
+    printAddition($number, $number1);
+    echo "<br>";
+    $resultado = addition(5, 9);
+    echo $resultado;
+    echo "<br>";
+    echo addition(33, 44);
+    echo "<br>";
+    echo "la suma 6 y 9 es addition(6,9)"; //asi no : la suma de 6 y 9 es addition(6m9)
+    echo "<br>";
+    echo "la suma 6 y 9 es " . addition(6, 9); //asi  si : suma de 6 y 9 es 15
+    
+    echo "<br>";
+    echo salute("juan");
+    echo "<br>";
+    echo "juan ", " buenos dias";
+    echo "<br>";
+
+    echo intoHem("hola");
+
+    echo matricula("mauricio", "  numansia");
+
+    echo matricula("kelvis", "  ciudad de jaen" , "Asir", "2018");
+
+    ?>
+    <h3>paramatros por valor o por referencia</h3>
+
+    <?php
+
+    function imcrement($n){
+        $n ++;
+        return $n;
+    }
+    $number = 8;
+    imcrement($number);
+    echo"<p>$number</p>"; // sale 8 porque pr defecto en las funciones se hace una copia del valor
+    // y no trabaja con la propia variavles son los parametros por valor (lotipico)
+    //$number = increment($num)
+
+
+    //para metros por referecia
+      function imcrementReference(&$n){
+        $n ++;
+        return $n;
+    }
+    $number = 8;
+    imcrementReference($number);
+    echo"<p>$number</p>"; 
+
+
+
+    
     ?>
 </body>
+
 </html>
