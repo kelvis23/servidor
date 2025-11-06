67b0928db4397e80c0f36bc5358c187ab9656b59
kelvis23 <jesuscuestas116@gmail.com>
2025-11-06 12:43:47 +0100

    metodos 2

--- php2/ejerciciosU2POO/clases/Empleade.php
+++ php2/ejerciciosU2POO/clases/Empleade.php
@@ -5,71 +5,101 @@ class Empleade
     private string $lestname;
     private float $salary = -1;
 
+    private array $telefono;
 
-    public function __construct(string $name, string $lestname, float $salary)
-    {
+
+    public function __construct(string $name, string $lestname,float $salary , array $telefono){
+    
         $this->name = $name;
         $this->lestname = $lestname;
-        $this->salary = $salary;
+        $this->salary =$salary;
+        $this->telefono = $telefono;
     }
-    public function getName()
-    {
+
+    public function getName() {
         return $this->name;
     }
 
-    public function getLestname()
-    {
+    public function getLestname(){
         return $this->lestname;
     }
-
-    public function getSalary()
-    {
+   public function getSalary(){
         return $this->salary;
     }
+    public function getTelefono(){
+        return $this->telefono;
+    }
+  
 
-    public function setName(string $name)
-    {
+    public function setName(string $name){
         $this->name = $name;
     }
 
-    public function setLestname(string $lestname)
-    {
+    public function setLestname(string $lestname){
         $this->lestname = $lestname;
     }
-
-    public function setSalary(float $salary)
-    {
-        $this->salary = $salary;
+ public function setSalary(float $salary) {
+       $this->salary= $salary;
+    }
+    public function setTelefono(array $telefono) {
+        $this->telefono = $telefono;
     }
 
-    public function __tostring()
-    {
+    public function __tostring(){
         return "<p> Nombre : " . $this->name . " Apellido: " . $this->lestname . " sueldo: " . $this->salary . "</p>";
     }
 
-    public function getNombreCompleto()
-    {
+    public function getNombreCompleto(){
         $res = " $this->name  $this->lestname";
         return $res;
     }
 
     public static function pagarImpuestos($salary){
-     $total = 0;
-     if($salary <=0){
-        $total = -1;
-     }else if ($salary <= 12450) {
-        $total = $salary * 0.19;
-    } else if ($salary <= 20200) {
-        $total = 12450 * 0.19 + ($salary - 12450) * 0.24;
-    } else if ($salary <= 35200) {
-        $total = 12450 * 0.19 + (20200 - 12450) * 0.24 + ($salary - 20200) * 0.30;
-    } else {
-        $total = 12450 * 0.19 + (20200 - 12450) * 0.24 + (35200 - 20200) * 0.30 + ($salary - 35200) * 0.37;
+        $total = 0;
+        if ($salary <= 0) {
+            $total = -1;
+        } else if ($salary <= 12450) {
+            $total = $salary * 0.19;
+        } else if ($salary <= 20200) {
+            $total = 12450 * 0.19 + ($salary - 12450) * 0.24;
+        } else if ($salary <= 35200) {
+            $total = 12450 * 0.19 + (20200 - 12450) * 0.24 + ($salary - 20200) * 0.30;
+        } else {
+            $total = 12450 * 0.19 + (20200 - 12450) * 0.24 + (35200 - 20200) * 0.30 + ($salary - 35200) * 0.37;
+        }
+
+        return round($total, 2);
     }
 
-    return round($total, 2);
-
-
+    public  function anadirTelefono(string $telefono){
+       $res = $this->telefono[]=$telefono;
+       return $res;
     }
 
+
+    public function  listarTelefonos(){
+        $fun[] = $this->telefono;
+
+        for ($i=0;$i < count($fun);$i++){
+           return implode(',',$fun[$i]);
+        }
+    }
+    public function  vaciarTelefonos (){
+         return $this->telefono = []; 
+         
+        }
+    
+    public function  toHtml(){
+        $salry = $this->salary;
+        $impuesto = self ::  pagarImpuestos($salry);
+        $res = "<p>Nombre: " . $this->name . 
+                " Apellido: " . $this->lestname . 
+                " Sueldo: " . $this->salary ." impuesto: $impuesto </p>";
+    echo $res;
+    echo "<ul>";
+    foreach ($this->telefono as $tel) {
+        echo "<li>$tel</li>";
+    }
+    echo "</ul>";
+    }
 }
--- php2/ejerciciosU2POO/index.php
+++ php2/ejerciciosU2POO/index.php
@@ -8,9 +8,23 @@
 <body>
     <?php
      include $_SERVER["DOCUMENT_ROOT"]."/php2/ejerciciosU2POO/clases/Empleade.php";
-    $n1 = new Empleade("kelvis" ,"moises",30000);
+    $n1 = new Empleade("kelvis" ,"moises",30000,[]);
      echo $n1->__tostring();
       echo Empleade::pagarImpuestos(30000);
+      echo"<br>";
+
+      $n2 = new Empleade("kelvis" ,"moises",30000,["12345","20708"]);
+      $n2->anadirTelefono("67890");
+      print_r($n2->getTelefono());
+    
+      echo"<br>";
+      echo $n2->listarTelefonos();
+            echo"<br>";
+      print_r($n2->vaciarTelefonos());
+
+         echo"<br>";
+         $n3 = new Empleade("kelvis" ,"moises",30000,["12345","20708","150604"]);
+          echo($n3->toHtml());
     ?>
 </body>
 </html>
