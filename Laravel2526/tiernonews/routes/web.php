6bd04fd6fbb6a51943c78e7c38d45187dc67cc84
kelvis23 <jesuscuestas116@gmail.com>
2026-01-27 12:44:47 +0100

    laravel

---
+++ Laravel2526/tiernonews/routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route ::get("/hola",function(){
+    return "hola mundo ";
+});
+
+
+Route ::get("/hola/{name}",function($name){
+    return "hola ,$name ";
+});
